50654a04edac537b7483dc9ff3cae507261a4a86
Wilson Jeronimo <wjsoliveira07@gmail.com>
2023-11-22 06:51:50 -0300

    Update ultima_versao.py

--- ultima_versao.py
+++ ultima_versao.py
@@ -1,46 +1,46 @@
 import pandas as pd
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
+from requests import get
 
-# URL_site_raspado
-url = input("Digite a URL do site a ser raspado: ")
+# URL do site raspado.
+url = input("Digite o link do site a ser raspado: ")
 
-# Abrir_URL_ler_conteúdo_HTML_página
-page = urlopen(url)
-html = page.read().decode("utf-8")
+# Leitura do HTML da página.
+response = get(url, headers={'Authorization': 'Bearer test_440825c49ff39c2785b97fc67b0609'})
+html = response.text
 
-# Criar_um_objeto_BeautifulSoup_para_analisar_HTML
+# Objeto para analisar o HTML.
 soup = BeautifulSoup(html, "html.parser")
 
-# Encontrar_tabelas_página
+# Encontrar tabelas na página.
 tabelas = soup.find_all('table')
 
-# Iterar_tabelas
+# Iterar sobre as tabelas encontradas.
 for i, tabela in enumerate(tabelas):
-    # Extrair dados da tabela e criar um DataFrame
+    # Dados da tabela e DataFrame
     data = []
     colunas = []
 
-    # Obter_nomes_colunas_tabela
+    # Nomes das colunas da tabela.
     for th in tabela.find_all('th'):
         colunas.append(th.text.strip())
 
-    # Certificar-se_de_que_existem_colunas_antes_de_tentar_extrair_dados
+    # Certificar-se de que existem colunas.
     if colunas:
-        for row in tabela.find_all('tr')[1:]:  # Ignorar a primeira linha se for o cabeçalho
+        for row in tabela.find_all('tr')[1:]:  # Ignorar a primeira somente se cabeçalho.
             columns = row.find_all(['td', 'th'])
             data.append([col.text.strip() for col in columns])
 
-        # Verificar_se_o_número_de_colunas_nos_dados_é_consistente_com_o_número_de_colunas_nas colunas
+        # Número de dados com o número de colunas.
         if len(colunas) != len(data[0]):
             print(f"Ajustando o número de colunas de {len(data[0])} para {len(colunas)}")
             data = [row[:len(colunas)] for row in data]
 
-        # Criar_um_DataFrame_com_os_dados_e_atribuir_nomes_às_colunas
+        # DataFrame com os dados e nomes das colunas.
         df = pd.DataFrame(data, columns=colunas[:len(data[0])])
 
-        # Exibir_o_DataFrame_com_os_nomes_reatribuídos
+        # DataFrame com os nomes reatribuídos
         print(f"\nDataFrame {i+1} - Nomes reatribuídos:")
-        print(df)
+        print(df.to_string(index=False))
     else:
         print(f"\nSem colunas encontradas na tabela {i+1}. Ignorando esta tabela.")
