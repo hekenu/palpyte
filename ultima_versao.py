e89fa66c55f8155c4e32fe3fc21935f9b50525f3
Wilson Jeronimo <wjsoliveira07@gmail.com>
2023-11-29 07:09:45 -0300

    Update ultima_versao.py

--- ultima_versao.py
+++ ultima_versao.py
@@ -1,32 +1,33 @@
 import pandas as pd
 from bs4 import BeautifulSoup
-from requests import get
+import requests
 
 # URL do site a ser raspado
 url = "https://p1.trrsf.com/api/musa-soccer/ms-standings-light?idChampionship=1398&idPhase=&language=pt-BR&country=BR&nav=N&timezone=BR"
 
-# Leitura do HTML da página.
+# Fazendo a solicitação HTTP e obtendo o HTML da página.
+response = requests.get(url)
 html = response.text
 
-# Objeto para analisar o HTML.
+# Obj para analisar o HTML.
 soup = BeautifulSoup(html, "html.parser")
 
-# Encontrar tabelas na página.
+# Encontrar tabs.
 tabelas = soup.find_all('table')
 
-# Iterar sobre as tabelas encontradas.
+# Iterar sobre tabs.
 for i, tabela in enumerate(tabelas):
-    # Dados da tabela e DataFrame
+    # Dados tab e DataFrame
     data = []
     colunas = []
 
-    # Nomes das colunas da tabela.
+    # Nomes colunas da tab.
     for th in tabela.find_all('th'):
         colunas.append(th.text.strip())
 
-    # Certificar-se de que existem colunas.
+    # Certificar existem colunas.
     if colunas:
-        for row in tabela.find_all('tr')[1:]:  # Ignorar a primeira somente se cabeçalho.
+        for row in tabela.find_all('tr')[1:]:  # Ignorar somente se cabeçalho.
             columns = row.find_all(['td', 'th'])
             data.append([col.text.strip() for col in columns])
 
@@ -35,7 +36,7 @@ for i, tabela in enumerate(tabelas):
             print(f"Ajustando o número de colunas de {len(data[0])} para {len(colunas)}")
             data = [row[:len(colunas)] for row in data]
 
-        # DataFrame com os dados e nomes das colunas.
+        # DataFrame dados e nomes colunas.
         df = pd.DataFrame(data, columns=colunas[:len(data[0])])
 
         # DataFrame com os nomes reatribuídos
